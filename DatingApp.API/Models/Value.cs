caa1ae9f7fbc16b39962b65b307efb9953ba5bb7
omerkurnhs <omer.kuroglu@nhs.net>
2019-07-04 11:40:04 +0100

    work commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        //type prop for shortcut
+        public int Id {get; set;}
+        public string Name { get; set; }
+    }
+}
