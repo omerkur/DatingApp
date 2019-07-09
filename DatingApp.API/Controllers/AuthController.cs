b0d7a345b9509b7afe8d5dc1f39a170a9aebb50a
omerkurnhs <omer.kuroglu@nhs.net>
2019-07-09 09:48:56 +0100

    End of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -32,7 +32,7 @@ namespace DatingApp.API.Controllers
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
-                return BadRequest("userForRegisterDto already exists");
+                return BadRequest("Username already exists");
 
             var userToCreate = new User
             {
@@ -49,35 +49,38 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
                 return Unauthorized();
 
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-                new Claim(ClaimTypes.Name, userFromRepo.Username)
-            };
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
 
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature); 
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature); 
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(1),
-                SigningCredentials = creds
-            };
+                var token = new JwtSecurityToken
+                (
+                    issuer: "localhost",
+                    audience: "localhost",
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
 
-            var tokenHandler = new JwtSecurityTokenHandler();
+                return Ok(new 
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return Ok(new {
-                token = tokenHandler.WriteToken(token)
-            });
+           
         }
 
     }
